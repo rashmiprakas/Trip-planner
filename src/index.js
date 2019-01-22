e81930ce4712cef7473a5c0c180f29a194eaecbd
Rashmi Prakash <rashmiprakash@Rashmis-MacBook-Pro.local>
2019-01-22 16:29:53 -0500

    build marker added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuZGVsaW9uZ2giLCJhIjoiY2pyODd4MjI3MDN6ZzQ5cGNtYndwY243MiJ9.LDGMSphA7rudj2j3DHIs0Q';;
 
@@ -9,6 +11,18 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
+// const markerDomEl = document.createElement("div");// should create it 
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+// //marker = new mapboxgl.Marker(marker).setLngLat([-74.009151, 40.705086]).addTo(map);
 
 
-console.log('hello from index.js!')
+
+
+
+//const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+//marker.addTo(map);
+buildMarker('activities', [-74.009151, 40.705086]).addTo(map);
+//console.log('hello from index.js!')
